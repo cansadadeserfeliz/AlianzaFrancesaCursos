661765998a9fa7a226a6ed602c7459ebaca6e243
cansadadeserfeliz <2270752+cansadadeserfeliz@users.noreply.github.com>
2023-09-03 02:20:20 +0000

    Add more level constants

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from termcolor import colored
 import requests
 from bs4 import BeautifulSoup
 
-place = 'CEDRITOS'
-place = None
-level = 'A2.1'
-intensity = 'SEMI-INTENSIVO (SABADOS)'
-places = ['CEDRITOS', 'CHICO', 'CENTRO']
+INTENCITY_SUPER_INTENCIVE = 'SUPER-INTENSIVO'
+INTENCITY_INTENCIVE = 'INTENSIVO'
+INTENCITY_WEEKDAYS = 'SEMI-INTENSIVO (SEMANA)'
+INTENCITY_SATURDAY = 'SEMI-INTENSIVO (SABADOS)'
+
+PLACE_CEDRITOS = 'CEDRITOS'
+PLACES = [PLACE_CEDRITOS, 'CHICO', 'CENTRO']
 
 COLOR_WHITE = 'white'
 COLOR_GREEN = 'green'
@@ -19,18 +21,34 @@ COLOR_LIGHT_BLUE = 'light_blue'
 COLOR_MAGENTA = 'magenta'
 COLOR_LIGHT_YELLOW = 'light_yellow'
 
+QUERY_PLACE = None
+QUERY_LEVEL = 'A2.1'
+QUERY_INTENSITY = INTENCITY_SATURDAY
+# QUERY_LEVEL = input('Qué curso quieres consultar? ')
+
 
 def process(text):
   text = re.sub(' +', ' ', text)
   return text.strip().replace('\n', '')
 
 
-if level.startswith('A1'):
-  course_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/a1/'
-elif level.startswith('A2'):
-  course_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/a2/'
+print(colored('Cursos para adultos presenciales', COLOR_LIGHT_YELLOW), 'nivel',
+      colored(QUERY_LEVEL, COLOR_RED), 'intensidad', QUERY_INTENSITY)
 
-response = requests.get(course_url)
+if QUERY_LEVEL.startswith('A1'):
+  query_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/a1/'
+elif QUERY_LEVEL.startswith('A2'):
+  query_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/a2/'
+elif QUERY_LEVEL.startswith('B1'):
+  query_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/b1/'
+elif QUERY_LEVEL.startswith('B2'):
+  query_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/b2/'
+elif QUERY_LEVEL.startswith('C1'):
+  query_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/c1/'
+elif QUERY_LEVEL.startswith('C2'):
+  query_url = 'https://alianzafrancesa.edu.co/bogota/cursos-y-examenes/c2/'
+
+response = requests.get(query_url)
 assert response.status_code == 200
 
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -39,19 +57,19 @@ courses = soup.select('.cursos .curso')
 for course in courses:
   content = course.text
 
-  if place and place not in content:
+  if QUERY_PLACE and QUERY_PLACE not in content:
     continue
 
   course_place = ''
-  for p in places:
+  for p in PLACES:
     if p in content:
       course_place = p
       break
 
-  if level not in course.get('data-nivel'):
+  if QUERY_LEVEL not in course.get('data-nivel'):
     continue
 
-  if intensity not in course.get('data-intensidad'):
+  if QUERY_INTENSITY not in course.get('data-intensidad'):
     continue
 
   print('--- === ---')
@@ -59,18 +77,23 @@ for course in courses:
   print('Intensidad:', colored(course.get('data-intensidad'), COLOR_BLUE))
   print('Cede:', colored(course_place, COLOR_MAGENTA))
   print('Formato:', colored(course.get('data-formato'), COLOR_LIGHT_BLUE))
-  print('Estado:', colored('Curso lleno', COLOR_RED) if 'Curso lleno' in content else colored('disponible', COLOR_GREEN))
+  print(
+    'Estado:',
+    colored('Curso lleno', COLOR_RED) if 'Curso lleno' in content else colored(
+      'disponible', COLOR_GREEN))
 
   for section in course.find_all('p'):
     strings = list(section.stripped_strings)
     if section.strong:
       title = process(section.strong.get_text())
-      
+
       value = ' '.join(section.find_all(string=True, recursive=False))
       value = process(value)
-      
+
       color = COLOR_WHITE
       if title == 'Formato:':
         color = COLOR_LIGHT_GREEN
-      
+
       print('\t', title, colored(value, color))
+
+print('FIN')
